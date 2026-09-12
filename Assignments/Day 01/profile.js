79052066abbe4c6fd7397f7fa683e6f113a207d0
SaadMMustafa <SaadMMustafa99@gmail.com>
2026-09-12 08:19:18 +0300

    12.9.2023

--- Assignments/Day 01/profile.js
+++ Assignments/Day 01/profile.js
@@ -1,10 +1,15 @@
-const myName = "Saad";
-const myCity = "Suez";
-const myPurpose = "I joined this track to start a new career";
+// const myName = "Saad";
+// const myCity = "Suez";
+// const myPurpose = "I joined this track to start a new career";
 
-function final(name, city, purpose) {
-    return `My name is ${name}. I live in ${city}. ${purpose}`;
-}
+// function final(name, city, purpose) {
+//     return `My name is ${name}. I live in ${city}. ${purpose}`;
+// }
 
-console.log(final(myName, myCity, myPurpose));
-console.log(`Node.js version: ${process.version}`);
+// console.log(final(myName, myCity, myPurpose));
+// console.log(`Node.js version: ${process.version}`);
+
+const title = document.getElementById('title');
+title.style.color = 'blue';
+title.textContent = 'New Name!';
+console.log(title);
