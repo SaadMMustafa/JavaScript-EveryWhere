a230eb813f5b41fcd83b50f3b0caed96224e13c8
SaadMMustafa <SaadMMustafa99@gmail.com>
2026-09-15 14:45:17 +0300

    02:45

--- Day 02/grade-engine.js
+++ Day 02/grade-engine.js
@@ -1,40 +1,22 @@
+let score = 55 ;
+let attendance = 60 ;
 
+if (attendance >= 80 && attendance <= 100) {
+    
+    if (score >= 70) {
+        console.log("Certificate awarded");
+    } else {
+        console.log("Review Needed");
+    };
 
-console.log("--------------");
+} else if (attendance < 80 && attendance >= 50) {
 
+    if (score >= 70) {
+        console.log("Certificate awarded");
+    } else {
+        console.log("Review Needed");
+    };
 
-
-console.log("--------------");
-
-
-
-console.log("--------------");
-
-
-
-console.log("--------------");
-
-
-
-console.log("--------------");
-
-
-
-console.log("--------------");
-
-
-
-console.log("--------------");
-
-
-
-console.log("--------------");
-
-
-
-console.log("--------------");
-
-
-
-console.log("--------------");
-
+} else {
+    console.log("Fail due to attendance");
+};
